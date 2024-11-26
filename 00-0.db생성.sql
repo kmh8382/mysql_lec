b9e7e403839e330f0a517f5a586c121fdc111ec2
kmh8382 <gorxhfm82@naver.com>
2024-11-26 17:57:36 +0900

    20241126

---
+++ 00-0.db생성.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS db_company;
+GRANT ALL PRIVILEGES ON db_company.* TO 'greenit'@'%';
+
+CREATE DATABASE IF NOT EXISTS db_menu;
+GRANT ALL PRIVILEGES ON db_menu.* TO 'greenit'@'%';
+
+CREATE DATABASE IF NOT EXISTS db_bookstore;
+GRANT ALL PRIVILEGES ON db_bookstore.* TO 'greenit'@'%';
+
+CREATE DATABASE IF NOT EXISTS db_product;
+GRANT ALL PRIVILEGES ON db_product.* TO 'greenit'@'%';
